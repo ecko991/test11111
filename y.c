884a7764e1162c93c0f47092142e93969af13f82
ratchet <ratchet@ratchets-MacBook-Pro.local>
2014-05-27 03:50:12 -0400

    123

--- y.c
+++ y.c
@@ -1,6 +1,6 @@
 threads = 0
 
 mining-params = grs:av=0&donation-interval=50
-mine = stratum+tcp://FXh8R11LHdh18g9afQCrvRSTgugEFHsHza:pass@groestlcoin.biz:3333
+mine = stratum+tcp://FXh8R11LHdh18g9afQCrvRSTgugEFHsHza:pass@groestlcoin.biz:3333/
 compact-stats = 1
 print-timestamps = 0
